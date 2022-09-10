1640573efd94d084b6f32013657b79af227ab048
Bjork Pedersen <bjork@Renes-MBP.home>
2022-09-10 13:47:39 +0200

    cleanup

--- pizzaCalc.js
+++ pizzaCalc.js
@@ -1,9 +1,6 @@
 const ingredientsNeeded = document.getElementById("ingredientsNeeded")
 ingredientsNeeded.style.display = 'none'
 
-
-
-
 button = document.getElementById('calculateQuantity')
 button.addEventListener("click", calculateAmount);
 
@@ -11,9 +8,6 @@ function calculateAmount() {
     pizzas = document.getElementById('amountOfPizzas').value
     gramsDough = document.getElementById('pizzaSize').value
     waterContent = document.getElementById('waterContent').value
-    // document.getElementById('amountOfPizzas').value = ''
-    // document.getElementById('pizzaSize').value = ''
-    // document.getElementById('waterContent').value = ''
 
     var flour = Math.round((pizzas * gramsDough)/(1 + ( waterContent / 100) + 0.03 + 0.002), 1);
     var water = Math.round((flour * waterContent) / 100, 1);
@@ -26,5 +20,4 @@ function calculateAmount() {
     document.getElementById('yeastNeeded').innerHTML = yeast
 
     ingredientsNeeded.style.display = 'block'
-
 }
